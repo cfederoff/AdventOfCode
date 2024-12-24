261b760d959da813a1ebe1eaad6d5ee650ef3173
Connor Federoff <51100944+Cfederoff@users.noreply.github.com>
2024-12-24 03:28:45 -0500

    Day 21

--- 2024/Day21/src/Puzzle2Day21.java
+++ 2024/Day21/src/Puzzle2Day21.java
@@ -3,29 +3,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
-public class Puzzle1Day21 {
+public class Puzzle2Day21 {
     static char[][] keypadVisual = {{'7','8','9'},{'4','5','6'},{'1','2','3'},{'.','0','A'}};
     static char[][] directionKeypadVisual = {{'.','^','A'},{'<','v','>'}};
     static ArrayList<String> currentPath = new ArrayList<>();
     static ArrayList<String>[][] keyPadPaths = new ArrayList[11][11];
 
     static ArrayList<String>[][] directionalKeyPadPaths = new ArrayList[5][5];
-    static int depth = 2;
+    static int depth = 25;
 
     public static void main(String[] args){
-        node[] keyPad = new node[11];
-        keyPad[0] = new node(1,3);
-        keyPad[1] = new node(0,2);
-        keyPad[2] = new node(1,2);
-        keyPad[3] = new node(2, 2);
-        keyPad[4] = new node(0,1);
-        keyPad[5] = new node(1,1);
-        keyPad[6] = new node(2,1);
-        keyPad[7] = new node(0,0);
-        keyPad[8] = new node(1,0);
-        keyPad[9] = new node(2,0);
-        keyPad[10] = new node(2,3);
+        Puzzle1Day21.node[] keyPad = new Puzzle1Day21.node[11];
+        keyPad[0] = new Puzzle1Day21.node(1,3);
+        keyPad[1] = new Puzzle1Day21.node(0,2);
+        keyPad[2] = new Puzzle1Day21.node(1,2);
+        keyPad[3] = new Puzzle1Day21.node(2, 2);
+        keyPad[4] = new Puzzle1Day21.node(0,1);
+        keyPad[5] = new Puzzle1Day21.node(1,1);
+        keyPad[6] = new Puzzle1Day21.node(2,1);
+        keyPad[7] = new Puzzle1Day21.node(0,0);
+        keyPad[8] = new Puzzle1Day21.node(1,0);
+        keyPad[9] = new Puzzle1Day21.node(2,0);
+        keyPad[10] = new Puzzle1Day21.node(2,3);
         for (int i = 0; i < keyPad.length; i++){
             for (int j = 0; j < keyPad.length; j++){
                 if (i == j){
@@ -43,7 +44,6 @@ public class Puzzle1Day21 {
                     for (int k = 0; k < currentPath.size(); k++){
                         if (currentPath.get(k).length() > minimumLength){
                             currentPath.remove(currentPath.get(k));
-                            k--;
                         }
                     }
                     keyPadPaths[i][j] = new ArrayList<>(currentPath);
@@ -51,12 +51,12 @@ public class Puzzle1Day21 {
                 }
             }
         }
-        node[] directionalKeypad = new node[5];
-        directionalKeypad[0] = new node(0,1); // <
-        directionalKeypad[1] = new node(1,1); // v
-        directionalKeypad[2] = new node(2,1); // >
-        directionalKeypad[3] = new node(1,0); // ^
-        directionalKeypad[4] = new node(2,0); // A
+        Puzzle1Day21.node[] directionalKeypad = new Puzzle1Day21.node[5];
+        directionalKeypad[0] = new Puzzle1Day21.node(0,1); // <
+        directionalKeypad[1] = new Puzzle1Day21.node(1,1); // v
+        directionalKeypad[2] = new Puzzle1Day21.node(2,1); // >
+        directionalKeypad[3] = new Puzzle1Day21.node(1,0); // ^
+        directionalKeypad[4] = new Puzzle1Day21.node(2,0); // A
         for (int i = 0; i < directionalKeyPadPaths.length; i++){
             for (int j = 0; j < directionalKeyPadPaths.length; j++){
                 if (i == j){
@@ -83,10 +83,6 @@ public class Puzzle1Day21 {
                 }
             }
         }
-        ArrayList<String> test = new ArrayList<>();
-
-        buildSequence("<A",0,'A',"",test);
-        HashMap<String, Long> hashMap = new HashMap<>();
         long total = 0;
         try {
             BufferedReader file = new BufferedReader(new FileReader("2024/Day21/InputDay21.txt"));
@@ -94,10 +90,10 @@ public class Puzzle1Day21 {
             while ((line = file.readLine()) != null){
                 ArrayList<String> sequence = new ArrayList<>();
                 buildKeypadSequence(line,0,'A',"", sequence);
+                HashMap<hashInfo, Long> hashMap = new HashMap<>();
                 long minimum = Long.MAX_VALUE;
-                HashMap<String, Long> hashmap = new HashMap<>();
                 for (int i = 0; i < sequence.size(); i++){
-                    long amount = shortestSequence(sequence.get(i),depth, hashmap);
+                    long amount = shortestSequence(sequence.get(i),depth, hashMap);
                     if (amount < minimum){
                         minimum = amount;
                     }
@@ -151,28 +147,28 @@ public class Puzzle1Day21 {
         }
         visited[y][x] = true;
         if (x-1 > -1 && !visited[y][x-1] && directionKeypadVisual[y][x-1] != '.'){
-            boolean[][] newVisited = new boolean[4][3];
+            boolean[][] newVisited = new boolean[2][3];
             for (int i = 0; i < 2; i++){
                 System.arraycopy(visited[i], 0, newVisited[i], 0, 3);
             }
             dfsDirectional(x-1,y,endX,endY, path + "<", newVisited);
         }
         if (y-1 > -1 && !visited[y-1][x] && directionKeypadVisual[y-1][x] != '.'){
-            boolean[][] newVisited = new boolean[4][3];
+            boolean[][] newVisited = new boolean[2][3];
             for (int i = 0; i < 2; i++){
                 System.arraycopy(visited[i], 0, newVisited[i], 0, 3);
             }
             dfsDirectional(x,y-1,endX,endY, path + "^", newVisited);
         }
         if (y+1 < 2 && !visited[y+1][x] && directionKeypadVisual[y+1][x] != '.'){
-            boolean[][] newVisited = new boolean[4][3];
+            boolean[][] newVisited = new boolean[2][3];
             for (int i = 0; i < 2; i++){
                 System.arraycopy(visited[i], 0, newVisited[i], 0, 3);
             }
             dfsDirectional(x,y+1,endX,endY, path + "v", newVisited);
         }
         if (x+1 < 3 && !visited[y][x+1] && directionKeypadVisual[y][x+1] != '.'){
-            boolean[][] newVisited = new boolean[4][3];
+            boolean[][] newVisited = new boolean[2][3];
             for (int i = 0; i < 2; i++){
                 System.arraycopy(visited[i], 0, newVisited[i], 0, 3);
             }
@@ -223,7 +219,7 @@ public class Puzzle1Day21 {
         }
         for (int i = 0; i < directionalKeyPadPaths[previousIndex][currentIndex].size(); i++){
             buildSequence(keys,index+1, currentChar,
-                    currPath + directionalKeyPadPaths[previousIndex][currentIndex].get(i) + 'A', result);
+                    currPath + directionalKeyPadPaths[previousIndex][currentIndex].get(i) + "A", result);
         }
 
     }
@@ -253,16 +249,21 @@ public class Puzzle1Day21 {
         }
     }
 
-    public static long shortestSequence(String keys, int depth, HashMap<String,Long> hashmap){
+    public static long shortestSequence(String keys, long depth, HashMap<hashInfo,Long> cache){
         if (depth == 0){
             return keys.length();
         }
-        if (hashmap.containsKey(keys + depth)){
-            return hashmap.get(keys + depth);
+        if (cache.containsKey(new hashInfo(keys,depth))){
+            return cache.get(new hashInfo(keys,depth));
         }
         String[] subKeys = keys.split("A");
-        for (int i = 0; i < subKeys.length; i++){
-            subKeys[i] = subKeys[i] + 'A';
+        if (subKeys.length == 0){
+            subKeys = new String[]{"A"};
+        }
+        else {
+            for (int i = 0; i < subKeys.length; i++) {
+                subKeys[i] = subKeys[i] + 'A';
+            }
         }
         long total = 0;
         for (int i = 0; i < subKeys.length; i++){
@@ -270,21 +271,35 @@ public class Puzzle1Day21 {
             buildSequence(subKeys[i],0,'A',"",sequenceList);
             long minimum = Long.MAX_VALUE;
             for (int j = 0; j < sequenceList.size(); j++){
-                if (shortestSequence(sequenceList.get(j),depth-1,hashmap) < minimum){
-                    minimum = shortestSequence(sequenceList.get(j),depth-1,hashmap);
+                long num = shortestSequence(sequenceList.get(j),depth-1, cache);
+                if (num < minimum){
+                    minimum = num;
                 }
             }
             total += minimum;
         }
-        hashmap.put(keys+depth,total);
+        cache.put(new hashInfo(keys,depth),total);
         return total;
     }
-    public static class node {
-        int x;
-        int y;
-        public node(int x, int y){
-            this.x = x;
-            this.y = y;
+    public static class hashInfo {
+        String keys;
+        Long total;
+        int hashcode;
+        hashInfo(String keys, Long total){
+            this.keys = keys;
+            this.total = total;
+            this.hashcode = Objects.hash(keys,total);
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof hashInfo HashInfo){
+                return this.keys.equals(HashInfo.keys) && Objects.equals(this.total, HashInfo.total);
+            }
+            return false;
+        }
+        @Override
+        public int hashCode() {
+            return this.hashcode;
         }
     }
 }
